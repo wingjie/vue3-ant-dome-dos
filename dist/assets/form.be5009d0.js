ce98d1feb1429d38902928d84568747bf35de427
liuweijie <liuweijie@nantian.com.cn>
2023-12-15 16:53:58 +0800

    打包

---
+++ dist/assets/form.be5009d0.js
@@ -0,0 +1,1 @@
+import{P as r}from"./index.c7b66487.js";function t(t){const n=t();if("array"===r(n)){const[r,t,o]=n;return{formData:r,fieldList:t,formModel:o}}return n}function n(r){return r()}export{n as a,t as d};
---
+++ dist/assets/table.ee01b210.js
@@ -0,0 +1,1 @@
+import{P as n}from"./index.c7b66487.js";function r(r){const t=r();if("array"===n(t)){const[n,r,a]=t;return{tableData:n,columns:r,fieldModel:a}}return t}function t(n){return n()}export{r as a,t as d};
