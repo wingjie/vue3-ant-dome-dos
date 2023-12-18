bb2931ba01ec4409b06c4e1fd4eb3335c5390e1c
liuweijie <liuweijie@nantian.com.cn>
2023-12-18 10:26:37 +0800

    sd

---
+++ dist/assets/form.6f12e716.js
@@ -0,0 +1,1 @@
+import{P as r}from"./index.af3feb7a.js";function t(t){const a=t();if("array"===r(a)){const[r,t,n]=a;return{formData:r,fieldList:t,formModel:n}}return a}function a(r){return r()}export{a,t as d};
---
+++ dist/assets/table.32e5a64b.js
@@ -0,0 +1,1 @@
+import{P as a}from"./index.af3feb7a.js";function n(n){const r=n();if("array"===a(r)){const[a,n,t]=r;return{tableData:a,columns:n,fieldModel:t}}return r}function r(a){return a()}export{n as a,r as d};
