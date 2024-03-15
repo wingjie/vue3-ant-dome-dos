c0ab8cf2a425c1828f5efdce456d59216bc25b8f
liuweijie <liuweijie@nantian.com.cn>
2024-03-15 18:16:46 +0800

    push

--- dist/assets/form.bddb4d68.js
+++
@@ -1,1 +0,0 @@
-import{P as r}from"./index.192a4aa9.js";function a(a){const t=a();if("array"===r(t)){const[r,a,n]=t;return{formData:r,fieldList:a,formModel:n}}return t}function t(r){return r()}export{t as a,a as d};
