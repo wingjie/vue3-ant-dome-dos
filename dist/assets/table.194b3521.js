43f40ade83bda49047a71563961d60ec903fa6b1
liuweijie <liuweijie@nantian.com.cn>
2024-03-15 18:08:53 +0800

    feat:push

---
+++ dist/assets/table.194b3521.js
@@ -0,0 +1,1 @@
+import{P as a}from"./index.192a4aa9.js";function n(n){const r=n();if("array"===a(r)){const[a,n,t]=r;return{tableData:a,columns:n,fieldModel:t}}return r}function r(a){return a()}export{n as a,r as d};
